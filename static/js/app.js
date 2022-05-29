958381785b69d7bec65e3a9dd99f1c7e5be4349c
LugalKiEnnn <iliy24ru@gmail.com>
2022-05-29 10:53:46 +0300

    second version

--- static/js/app.js
+++ static/js/app.js
@@ -27,52 +27,155 @@ function init() {
             // от 0 (весь мир) до 19.
             zoom: 9,
             controls: ['zoomControl']
-        }, {
-            searchControlProvider: 'yandex#search'
         }),
-        objects = ymaps.geoQuery([
-            {
-                type: 'Point',
-                coordinates: [55.73, 37.75]
+        ListBoxLayout = ymaps.templateLayoutFactory.createClass(
+            "<button id='my-listbox-header' class='btn btn-success dropdown-toggle' data-toggle='dropdown' style='display: none'>" +
+                "{{data.title}} <span class='caret'></span>" +
+            "</button>" +
+            // Этот элемент будет служить контейнером для элементов списка.
+            // В зависимости от того, свернут или развернут список, этот контейнер будет
+            // скрываться или показываться вместе с дочерними элементами.
+            "<ul id='my-listbox'" +
+                " class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu'" +
+                " style='display: {% if state.expanded %}block{% else %}none{% endif %};'></ul>", {
+
+            build: function() {
+                // Вызываем метод build родительского класса перед выполнением
+                // дополнительных действий.
+                ListBoxLayout.superclass.build.call(this);
+
+                this.childContainerElement = $('#my-listbox').get(0);
+                // Генерируем специальное событие, оповещающее элемент управления
+                // о смене контейнера дочерних элементов.
+                this.events.fire('childcontainerchange', {
+                    newChildContainerElement: this.childContainerElement,
+                    oldChildContainerElement: null
+                });
             },
-            {
-                type: 'Point',
-                coordinates: [55.10, 37.45]
+
+            // Переопределяем интерфейсный метод, возвращающий ссылку на
+            // контейнер дочерних элементов.
+            getChildContainerElement: function () {
+                return this.childContainerElement;
             },
-            {
-                type: 'Point',
-                coordinates: [55.25, 37.35]
+
+            clear: function () {
+                // Заставим элемент управления перед очисткой макета
+                // откреплять дочерние элементы от родительского.
+                // Это защитит нас от неожиданных ошибок,
+                // связанных с уничтожением dom-элементов в ранних версиях ie.
+                this.events.fire('childcontainerchange', {
+                    newChildContainerElement: null,
+                    oldChildContainerElement: this.childContainerElement
+                });
+                this.childContainerElement = null;
+                // Вызываем метод clear родительского класса после выполнения
+                // дополнительных действий.
+                ListBoxLayout.superclass.clear.call(this);
             }
-        ]).addToMap(myMap),
-        circle = new ymaps.Circle([[55.43, 37.7], 10000], null, { draggable: true });
-        
-    circle.events.add('drag', function () {
-        // Объекты, попадающие в круг, будут становиться красными.
-        var objectsInsideCircle = objects.searchInside(circle);
-        objectsInsideCircle.setOptions('preset', 'islands#redIcon');
-        // Оставшиеся объекты - синими.
-        objects.remove(objectsInsideCircle).setOptions('preset', 'islands#blueIcon');
-    });
-    myMap.geoObjects.add(circle);
+        }),
+
+        // Также создадим макет для отдельного элемента списка.
+        ListBoxItemLayout = ymaps.templateLayoutFactory.createClass(
+            "<li><a>{{data.content}}</a></li>"
+        ),
+
+        // Создадим 2 пункта выпадающего списка
+        listBoxItems = [
+            new ymaps.control.ListBoxItem({
+                data: {
+                    content: 'Москва',
+                    center: [55.751574, 37.573856],
+                    zoom: 9
+                }
+            }),
+            new ymaps.control.ListBoxItem({
+                data: {
+                    content: 'Санкт-Петербург',
+                    center: [59.94, 30.32],
+                    zoom: 9
+                }
+            }),
+            new ymaps.control.ListBoxItem({
+                data: {
+                    content: 'Новосибирск',
+                    center: [54.990215, 73.365535],
+                    zoom: 9
+                }
+            })
+        ],
+
+        // Теперь создадим список, содержащий 2 пункта.
+        listBox = new ymaps.control.ListBox({
+                items: listBoxItems,
+                data: {
+                    title: 'Выберите город'
+                },
+                options: {
+                    // С помощью опций можно задать как макет непосредственно для списка,
+                    layout: ListBoxLayout,
+                    // так и макет для дочерних элементов списка. Для задания опций дочерних
+                    // элементов через родительский элемент необходимо добавлять префикс
+                    // 'item' к названиям опций.
+                    itemLayout: ListBoxItemLayout
+                }
+            });
+
+        listBox.events.add('click', function (e) {
+            // Получаем ссылку на объект, по которому кликнули.
+            // События элементов списка пропагируются
+            // и их можно слушать на родительском элементе.
+            var item = e.get('target');
+            // Клик на заголовке выпадающего списка обрабатывать не надо.
+            if (item != listBox) {
+                myMap.setCenter(
+                    item.data.get('center'),
+                    item.data.get('zoom')
+                );
+            }
+        });
+    myMap.controls.add(listBox, {float: 'left'});
+    selectTown();
 }
 
-function openTownList() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    myMap.geoObjects.add(new ymaps.Circle([[55.43, 37.7], 10000], null, { draggable: true }));
-  }
-  
-  // Закройте выпадающее меню, если пользователь щелкает за его пределами
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+const selectTown = function () {
+    var selectItem = document.getElementsByClassName("town_item");
+    
+    for(var i = 0; i < selectItem.length; i++) {
+        selectItem[i].addEventListener('click', selectChoose);
+    }
+
+    function selectChoose() {
+        var text = this.innerText;
+        var buttonSelectTown = document.getElementsByClassName("dropbtn")[0];
+        
+        buttonSelectTown.innerText = text;
+
+        var listbox = document.getElementById("my-listbox");
+        var townRefs = listbox.getElementsByTagName("a");
+
+        for(var i = 0; i < townRefs.length; i++) {
+            if(townRefs[i].innerText === text) {
+                townRefs[i].click();
+            }
         }
+
+    }
+};
+
+function openTownList() {
+    document.getElementById("myDropdown").classList.toggle("show");    
+}
+  
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
-
-  alert(JSON.stringify(townsCoordinates));
+};
